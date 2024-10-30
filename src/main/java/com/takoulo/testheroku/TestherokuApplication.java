9184a6cba4b4798f825bdf7e60e548674ded0ca4
Armel TAKOULO <tfarmel@yahoo.fr>
2024-10-30 10:32:47 +0100

    ajout des sources

---
+++ src/main/java/com/takoulo/testheroku/TestherokuApplication.java
@@ -0,0 +1,13 @@
+package com.takoulo.testheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestherokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestherokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takoulo/testheroku/TestherokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takoulo.testheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestherokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
