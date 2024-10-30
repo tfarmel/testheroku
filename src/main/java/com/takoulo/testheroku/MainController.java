753bc63ce9f27e5db26466f0bc970033686c4c73
Armel TAKOULO <tfarmel@yahoo.fr>
2024-10-30 14:14:40 +0100

    ajout du system properties pour java 17

--- src/main/java/com/takoulo/testheroku/MainController.java
+++ src/main/java/com/takoulo/testheroku/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
 
-    @GetMapping(value = {"", "/", "/index"})
+    @GetMapping(value={"", "/", "/index"})
     public String index(){
         return "/index";
     }
