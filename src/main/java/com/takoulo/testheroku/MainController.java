7f385127f84a1abadabe2df34527f8ae516a9d14
Armel TAKOULO <tfarmel@yahoo.fr>
2024-10-30 11:42:14 +0100

    page d'accueil

---
+++ src/main/java/com/takoulo/testheroku/MainController.java
@@ -0,0 +1,16 @@
+package com.takoulo.testheroku;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+
+    @GetMapping(value = {"", "/", "/index"})
+    public String index(){
+        return "/index";
+    }
+
+
+}
